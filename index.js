adac4ac43ab5223a56e7ca3c65bf7861d026cdde
m0x31h0x64i <m0x31h0x64i@gmail.com>
2022-09-17 16:20:26 +0430

    v3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = require('express')()
 const cors = require('cors')
