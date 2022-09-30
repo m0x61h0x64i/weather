dc6a91af8ffdc640ff0c618ce3a6990a52266579
m0x31h0x64i <m0x31h0x64i@gmail.com>
2022-09-30 21:39:25 +0330

    v5

--- APIs/weather.api.js
+++ APIs/weather.api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const forecast = async(address, callback) => {
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${address}&appid=${process.env.OWM_ACCESS_TOKEN}`
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${address}&appid=${process.env.OWM_ACCESS_TOKEN}`
     try {
         const response = await axios({ method: 'get', url, headers: { 'Content-Type': 'application/json' } })
         return callback(null, response)
